604485a08644e137205d95d306218c7b65ac9c06
TanapornSrisawan <tanaporn.srisa@gmail.com>
2023-03-11 13:36:44 +0700

    send assignment

---
+++ age Calculator.js
@@ -0,0 +1,10 @@
+// Store your birth year in a variable.<br>
+// Store a future year in another variable. <br>
+// Calculate your possible ages for the future year and display it in the console. <br>
+// Example: If you were born in 1988, then in 2026 you’ll be 37 or 38 (depending on the month in 2026).
+
+
+let birthYear = 1996;
+let futureYear = 2025 ;
+let futureAge = futureYear -  birthYear;
+console.log(`I was born in ${birthYear} , then in ${futureYear} I will be ${futureAge}`);
