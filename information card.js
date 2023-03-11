853cceecf96640913bb32dd99bf9bdaaa772ddb7
TanapornSrisawan <tanaporn.srisa@gmail.com>
2023-03-11 13:38:39 +0700

    Challenge 2: Create an information card

---
+++ information card.js
@@ -0,0 +1,12 @@
+/*`Create a card holding 3 pieces of information about a person, such as Name, Location, and Hobby.
+Each piece of information should be a variable.
+Display the card with the information in the correct place.
+Example: “Hi, my name is John. I live in South Africa and enjoy drinking homemade wine.”`*/
+
+let aboutme ={
+    name:'Ribbon',
+    location:'Vancouver,Canada',
+    hobby:'enjoy eating'
+};
+console.log(`Hi myname is ${aboutme.name}. I live in ${aboutme.location} and ${aboutme.hobby}.`);
+
